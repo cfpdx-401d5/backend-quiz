e0edc5460415b3c00d3a43416b5be44cfc325226
sdcaulley <stephanie.caulley@gmail.com>
2017-02-27 10:51:01 -0800

    schema tests done

--- test/models/images-schema.test.js
+++ test/models/images-schema.test.js
@@ -1,5 +1,21 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
-const assert = chai.assert;
-const app = require('../../app');
+const assert = require('chai').assert;
+const Image = require('../../lib/models/images-schema');
+const testInvalid = require('./test-invalid')(Image);
+
+describe('image model', () => {
+    it('requires a title', () => {
+        return testInvalid({ description: 'a beautiful sunset', category: 'places', url: '/directory/file.jpg' });
+    });
+
+    it('requires a category', () => {
+        return testInvalid({ title: 'Sunset on the Zambezi', description: 'a beautiful sunset', url: '/directory/file.jpg' });
+    });
+
+    it('requires a enumerated category', () => {
+        return testInvalid({ title: 'Sunset on the Zambezi', description: 'a beautiful sunset', category: 'childhood', url: '/directory/file.jpg' });
+    });
+
+    it('requirs a url', () => {
+        return testInvalid({ title: 'Sunset on the Zambezi', description: 'a beautiful sunset', category: 'places' });
+    });
+});
