70587ee5bfdd758e26032a6d3e6902a98c9cd6db
Brigitte Huneke <brigittehuneke@gmail.com>
2017-02-27 10:28:44 -0800

    got basic server up and running

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require('./lib/app');
+const http = require('http');
+
+require('./lib/connection');
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log('Server is running on :', server.address())
+});
