236c5b41245a30fef4d206f21302a277d03298e9
meghanroserebecca <meghanroserebecca@gmail.com>
2017-02-27 10:21:42 -0800

    base files for managing dependincies, .gitignore, etc. created: server.js written

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const app = require('./lib/app');
+require('mongoose');
+
+require('./lib/connection');
+
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log('server is running on ', server.address());
+});
