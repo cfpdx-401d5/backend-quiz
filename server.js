0c596f777fb9c1a677869486facef69932b4dcb0
Lynn <code@krogseng.com>
2017-02-27 11:13:29 -0800

    Base files and servers.

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require('./lib/app');
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+require('./lib/connection');
+const server = http.createServer(app);
+server.listen(PORT, () => {
+    console.log('server is running on ', server.address());
+});
---
+++ test/imageapis.test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const app = require('../lib/app');
+const assert = chai.assert;
+
+
+describe('build an image list api', (done) => {
+    it('post an image', () => {
+        console.log('in test');
+        assert.equal(1, 1);
+        return (done);
+    })
+});
